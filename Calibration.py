3be6b0d3936b8f73f04b0dfc8489089e0f1d6469
julienbernat <jul.b@outlook.com>
2023-02-27 14:56:07 -0500

    Clean Calibration

--- Calibration.py
+++ Calibration.py
@@ -2,26 +2,41 @@ import numpy as np
 import cv2 as cv
 import glob
 
+stereoChessboards = glob.glob("./StereoChessboards/*")
+
+#3D points of chessboard
 objPts = []
+
+#2D points of left and right image
 imgPtsL = []
 imgPtsR = []
 
+#Chssboard size
 cbSize = (5, 7)
-imgSize = [1314, 1027]
-stereoChessboards = glob.glob("./StereoChessboards/*")
-
 criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
-objP = np.zeros((cbSize[0]*cbSize[1], 3), np.float32)
 
-objP[:, :2] = np.mgrid[0:cbSize[0], 0:cbSize[1]].T.reshape(-1, 2)*20
+#Image size
+imgSize = [1314, 1027]
+
+#World coordinates for 3D points
+coord = np.zeros((cbSize[0]*cbSize[1], 3), np.float32)
+coord[:, :2] = np.mgrid[0:cbSize[0], 0:cbSize[1]].T.reshape(-1, 2)
+
+def getIntrinsicMatrix(imgPts):
+    retval, cameraMatrix, distCoeffs, rvecs, tvecs = cv.calibrateCamera(objPts, imgPts, imgSize, None, None)
+    optimalCameraMatrix, roi_L = cv.getOptimalNewCameraMatrix(cameraMatrix, distCoeffs, (imgSize[0], imgSize[1]), 1, (imgSize[0], imgSize[1]))
+    return  optimalCameraMatrix , distCoeffs
+
 
 def CalibrateCamera():
-
     for chessBoard in stereoChessboards:
         img = cv.imread(chessBoard)
+
+        #Separate left and right image
         imgL = img[:, :int(len(img[0])/2)]
         imgR = img[:, int(len(img[0])/2):]
 
+        #Find corners of the chessboard
         grayL = cv.cvtColor(imgL, cv.COLOR_BGR2GRAY)
         foundL, cornersL = cv.findChessboardCorners(grayL, cbSize, None)
 
@@ -29,34 +44,29 @@ def CalibrateCamera():
         foundR, cornersR = cv.findChessboardCorners(grayR, cbSize, None)
 
         if foundL and foundR== True:
-
-            objPts.append(objP)
-
+            objPts.append(coord)
+            
+            #Add 2d image points of the chessboard corners
             cv.cornerSubPix(grayL, cornersL, (11, 11), (-1, -1), criteria)
             imgPtsL.append(cornersL)
             
             cv.cornerSubPix(grayR, cornersR, (11, 11), (-1, -1), criteria)
             imgPtsR.append(cornersR)
 
-    retL, cameraMatrixL, distL, rvecsL, tvecsL = cv.calibrateCamera(
-        objPts, imgPtsL, imgSize, None, None)
-    newCameraMatrixL, roi_L = cv.getOptimalNewCameraMatrix(
-        cameraMatrixL, distL, (imgSize[0], imgSize[1]), 1, (imgSize[0], imgSize[1]))
-
-    retR, cameraMatrixR, distR, rvecsR, tvecsR = cv.calibrateCamera(
-        objPts, imgPtsR, imgSize, None, None)
-    newCameraMatrixR, roi_R = cv.getOptimalNewCameraMatrix(
-        cameraMatrixR, distR, (imgSize[0], imgSize[1]), 1, (imgSize[0], imgSize[1]))
+    #Calculate intrisic matrices
+    intrinsicL, distL = getIntrinsicMatrix(imgPtsL)
+    intrinsicR, distR = getIntrinsicMatrix(imgPtsR)
 
     flags = 0
     flags |= cv.CALIB_FIX_INTRINSIC
 
-    retStereo, newCameraMatrixL, distL, newCameraMatrixR, distR, rot, trans, essentialMatrix, fundamentalMatrix = cv.stereoCalibrate(
-        objPts, imgPtsL, imgPtsR, newCameraMatrixL, distL, newCameraMatrixR, distR, (imgSize[0], imgSize[1]), criteria, flags)
+    #Calculate fundamental and essential matrix with stereo calibration
+    retStereo, intrinsicL, distL, intrinsicR, distR, rot, trans, essentialMatrix, fundamentalMatrix = cv.stereoCalibrate(
+        objPts, imgPtsL, imgPtsR, intrinsicL, distL, intrinsicR, distR, (imgSize[0], imgSize[1]), criteria, flags)
 
     print("retval:\n", retStereo)
-    print("\nRight camera intrinsic:\n", newCameraMatrixR)
-    print("\nLeft camera intrinsic:\n", newCameraMatrixL)
+    print("\nRight camera intrinsic matrix:\n", intrinsicR)
+    print("\nLeft camera intrinsic matrix:\n", intrinsicL)
     print("\nFundamental:\n", fundamentalMatrix)
     print("\nEssential:\n", essentialMatrix)
     print("\nTranslation:\n", trans)
@@ -64,4 +74,4 @@ def CalibrateCamera():
     print("\nDistorsion left:\n", distL)
     print("\nDistorsion right:\n", distR)
 
-    return fundamentalMatrix, newCameraMatrixL
+    return fundamentalMatrix, intrinsicL
