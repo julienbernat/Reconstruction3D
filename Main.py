78eb639ecaaf159df9c5109ccf8e187feed0d6e7
afsaneh <afsaneh1994@gmail.com>
2023-03-06 16:41:53 -0500

    remove commented stuff

--- Main.py
+++ Main.py
@@ -17,30 +17,5 @@ if __name__ == "__main__":
     
     CalculateDisparity(parser.parse_args().file_number,
                    intrinsicL, intrinsicR, distL, distR)
-    
-
-    # imgL = cv.imread('./StereoImages/tsukuba_l.png', cv.IMREAD_GRAYSCALE)
-    # imgR = cv.imread('./StereoImages/tsukuba_r.png', cv.IMREAD_GRAYSCALE)
-    # stereo = cv.StereoBM_create(numDisparities=32, blockSize=17)
-    # disparity = stereo.compute(imgL,imgR)
-
-    # h, w = imgL.shape[:2]
-    # f = 0.8 * w  # guess for focal length
-    # Q = np.float32([[1, 0, 0, -0.5 * w],
-    #                 [0, -1, 0, 0.5 * h],  # turn points 180 deg around x-axis,
-    #                 [0, 0, 0, -f],  # so that y-axis looks up
-    #                 [0, 0, 1, 0]])
-    # points = cv.reprojectImageTo3D(disparity, Q)
-    # colors = cv.cvtColor(imgL, cv.COLOR_BGR2RGB)
-    # mask = disparity > disparity.min()
-    # out_points = points[mask]
-    # out_colors = colors[mask]
-    # disparity_scaled = (disparity - 16) / 32
-    # disparity_scaled += abs(np.amin(disparity_scaled))
-    # disparity_scaled /= np.amax(disparity_scaled)
-    # disparity_scaled[disparity_scaled < 0] = 0
-    # depth = np.array(255 * disparity_scaled, np.uint8)
-    # plt.imshow(depth,'gray')
-    # plt.show()
 
 
